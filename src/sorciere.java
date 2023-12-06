2eabec597b121be66bc1dacf09be689fe651576a
Mathias.G <77049177+Mathias-Gh@users.noreply.github.com>
2023-12-06 14:53:19 +0100

    Finishing Story4 Create polymorphism and call in main.

--- src/sorciere.java
+++ src/sorciere.java
@@ -1,7 +1,19 @@
-public class sorciere extends Personnage{
+public class sorciere extends Ennemi{
     public String description;
     public boolean peutInvoquerSquelettes;
+
+    public sorciere(String nom, int pointsDeVie, int force) {
+        super(nom, pointsDeVie, force);
+    }
+
     public void invoquer(){
 
+    }
+    public void attaquer() {
+        super.attaquer();
+    }
+    public void profil () {
+        System.out.println("La sorcière s'appelle " + nom + " il a " +
+                pointsDeVie + " points de vie " + "et il a " + force + " de force ");
     }
 }
