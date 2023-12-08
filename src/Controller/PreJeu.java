fa811f016658d95f75bb5039982a36af986a57fa
Youtmax <youtmax654@gmail.com>
2023-12-08 13:02:28 +0100

    Try catch added

--- src/Controller/PreJeu.java
+++ src/Controller/PreJeu.java
@@ -5,19 +5,34 @@ import Utils.ChargerJoueur;
 import Utils.Console;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PreJeu {
     public static void start() {
+        Scanner scanner = new Scanner(System.in);
+        int optionJeu;
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             Console.clear(); // Efface la console (le terminal)
             System.out.println("Menu du jeu:");
             System.out.println("1. Créer un personnage");
             System.out.println("2. Charger un personnage");
             System.out.println("3. Quitter");
-            System.out.println("Choisissez une option:");
-            int optionJeu = scanner.nextInt();
+            System.out.println("Choisissez une option (1-3):");
+            try {
+                // Vérifier si l'entrée est un nombre
+                if (scanner.hasNextInt()) {
+                    optionJeu = scanner.nextInt();
+                } else {
+                    System.out.println("Veuillez entrer un nombre.");
+                    sleep(3000);
+                    scanner.next(); // Consommer l'entrée invalide
+                    continue;
+                }
+            } catch (InputMismatchException e) {
+                e.printStackTrace(); // Afficher les détails de l'exception pour le débogage
+                continue;
+            }
 
             if (optionJeu == 1) {
                 System.out.println("Quel est le nom de votre personnage ?");
@@ -30,14 +45,25 @@ public class PreJeu {
                 Jeu.start(Player);
                 break;
             }
-            if (optionJeu == 2) {
+            else if (optionJeu == 2) {
                 Console.clear(); // Efface la console (le terminal)
                 Jeu.start(ChargerJoueur.start());
                 break;
             }
-            if (optionJeu == 3) {
+            else if (optionJeu == 3) {
                 break;
             }
+            else {
+                System.out.println("Cette option n'existe pas. Veuillez entrer une option valide");
+                sleep(2000);
+            }
+        }
+    }
+    public static void sleep(int miliseconds) {
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,33 @@ import Utils.Console;
 import Controller.PreJeu;
 import Controller.Combat;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int option;
         while (true) {
             Console.clear(); // Efface la console (le terminal)
             System.out.println("Menu principal:");
             System.out.println("1. Jouer");
             System.out.println("2. Quitter");
-            System.out.println("Choisissez une option:");
-            int option = scanner.nextInt();
+            System.out.println("Choisissez une option (1-2):");
+            try {
+                // Vérifier si l'entrée est un nombre
+                if (scanner.hasNextInt()) {
+                    option = scanner.nextInt();
+                } else {
+                    System.out.println("Veuillez entrer un nombre.");
+                    sleep(3000);
+                    scanner.next(); // Consommer l'entrée invalide
+                    continue;
+                }
+            } catch (InputMismatchException e) {
+                e.printStackTrace(); // Afficher les détails de l'exception pour le débogage
+                continue;
+            }
 
             if (option == 1) {
                 PreJeu.start();
@@ -24,7 +39,15 @@ public class Main {
             }
             else {
                 System.out.println("Cette option n'existe pas. Veuillez entrer une option valide");
+                sleep(2000);
             }
+        }
+    }
+    public static void sleep(int miliseconds) {
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
